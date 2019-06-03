8856414933c81589cee3e5b9ab3104606addaf31
mislavivancic <mislav.ivancic@fer.hr>
2019-06-03 20:32:53 +0200

    Added security

---
+++ src/main/java/com/example/security/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("allJokes");
+        registry.addViewController("/new").setViewName("newJoke");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
